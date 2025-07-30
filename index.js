59b3047f9a247636ffb245a6d241c7f17c78eeeb
AdissonValentim <169061416+adissonvalentimbrandao@users.noreply.github.com>
2025-07-30 00:45:34 -0300

    ajsute-testes

---
+++ index.js
@@ -0,0 +1,13 @@
+function fetchData(callback) {
+  const xhr = new XMLHttpRequest();
+  xhr.open("GET", "https://api.example.com/data");
+  xhr.onload = function () {
+    if (xhr.status === 200) {
+      const data = JSON.parse(xhr.responseText);
+      callback(data);
+    } else {
+      console.error("Erro na requisição");
+    }
+  };
+  xhr.send();
+}
